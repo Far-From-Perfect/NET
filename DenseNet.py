8dc20f64005703b08c4d37e9e149d16e032ce145
Stanislaw <97619562+Far-From-Perfect@users.noreply.github.com>
2022-08-03 00:47:00 +0300

    Attempt to implement DenseNet
    88% при 10 epochs за 1 час 10 мин на Датасете Симпсонов

---
+++ DenseNet.py
@@ -0,0 +1,88 @@
+import torch
+import torchvision
+from torch import nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+
+class _DenseLayer(nn.Sequential):
+    def __init__(self, num_input_features, growth_rate, bn_size, drop_rate):
+        super(_DenseLayer, self).__init__()
+        self.add_module('norm1', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu1', nn.ReLU(inplace=True))
+        self.add_module('conv1', nn.Conv2d(num_input_features, bn_size*growth_rate,
+                                           kernel_size=1, stride=1, bias=False))
+        self.add_module('norm2', nn.BatchNorm2d(bn_size*growth_rate))
+        self.add_module('relu2', nn.ReLU(inplace=True))
+        self.add_module('conv2', nn.Conv2d(bn_size*growth_rate, growth_rate, kernel_size=3, stride=1,
+                                           padding=1, bias=False))
+        self.drop_rate = drop_rate
+
+    def forward(self, x):
+        new_features = super(_DenseLayer, self).forward(x)
+        if self.drop_rate > 0:
+            new_features = F.dropout(new_features, p=self.drop_rate, training=self.training)
+        return torch.cat([x, new_features], 1)
+
+
+class _DenseBlock(nn.Sequential):
+    def __init__(self, num_layers, num_input_features, bn_size, growth_rate, drop_rate):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(num_input_features+i*growth_rate, growth_rate, bn_size, drop_rate)
+            self.add_module(f'denselayer_{i+1}', layer)
+
+
+class _Transition(nn.Sequential):
+    def __init__(self, num_input_features, num_output_features):
+        super(_Transition, self).__init__()
+        self.add_module('norm', nn.BatchNorm2d(num_input_features))
+        self.add_module('relu', nn.ReLU(inplace=True))
+        self.add_module('conv', nn.Conv2d(num_input_features, num_output_features, kernel_size=1, stride=1, bias=False))
+        self.add_module('pool', nn.AvgPool2d(2, stride=2))
+
+
+class DenseNet(nn.Module):
+    def __init__(self, growth_rate=32, block_config=(6, 12, 24, 16), num_init_features=64, bn_size=4,
+                 compression_rate=0.5, drop_rate=0, num_classes=1000):
+        super(DenseNet, self).__init__()
+        self.features = nn.Sequential(OrderedDict([
+            ('conv0', nn.Conv2d(3, num_init_features, kernel_size=7, stride=2, padding=3, bias=False)),
+            ('norm0', nn.BatchNorm2d(num_init_features)),
+            ('relu0', nn.ReLU(inplace=True)),
+            ('pool0', nn.MaxPool2d(3, stride=2, padding=1))
+        ]))
+
+        num_features = num_init_features
+        for i, num_layers in enumerate(block_config):
+            block = _DenseBlock(num_layers, num_features, bn_size, growth_rate, drop_rate)
+            self.features.add_module(f'DenseBlock_{i+1}', block)
+            num_features += num_layers * growth_rate
+            if i != len(block_config) - 1:
+                transition = _Transition(num_features, int(num_features*compression_rate))
+                self.features.add_module(f'Transition_{i+1}', transition)
+                num_features = int(num_features * compression_rate)
+
+        self.features.add_module('norm5', nn.BatchNorm2d(num_features))
+        self.features.add_module('relu5', nn.ReLU(inplace=True))
+
+        self.classifier = nn.Linear(9216, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1)
+            elif isinstance(m, nn.Linear):
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        features = self.features(x)
+        out = F.avg_pool2d(features, 7, stride=1).view(features.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+
+densenet = DenseNet(num_classes=42)
+# Модель выдала 88% на 10 эпохах(хахаххх) за 1:09:37
